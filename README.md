d65f0e27ed375b86e9766e18fd848b994f6dca84
Manuela <manuela.a@alunoescolassociais.org.br>
2024-10-25 11:57:34 -0300

    Adiciona o resto do código do JavaScript, com as variáveis que converte o valor de uma moeda para outra

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# conversor-de-moeda
+# Projeto Conversor de Moeda 
+
+## ÍNDICE 
+
+* [Introdução](#introdução)
+* [Descrição](#descrição)
+* [Funcionalidades](#funcionalidades)
+* [Resultado](#resultado)
+* [Fontes](#fontes)
+* [Autora](#autora)
+* [Orientador](#orientador)
+
--- conversor.js
+++ conversor.js
@@ -1,16 +1,25 @@
 document.getElementById('currency-form').addEventListener('submit', function(event){
-
-    //obter valores de entrada do formulário
-    const valor = parseFloat(document.getElementById('amount').value);
-    const daMoeda = document.getElementById('daMoeda').value;
-    const paraMoeda = document.getElementById('paraMoeda').value;
-
+ 
+    event.preventDefault();
+ 
+    //obter valores de entrada do formulário 
+    const valor = parseFloat (document.getElementById('amount').value);
+    const damoeda = document.getElementById("damoeda").value;
+    const paramoeda = document.getElementById("paramoeda").value;
+ 
     //definir taxa de câmbio fixa com utilização de objeto
     const exchangeRates = {
-        USD: { EUR: 0.93, BRL: 5.71 },
-        BRL: { EUR: 0.16, USD: 0.18 },
-        EUR: { BRL: 6.16, USD: 1.08 }
+        USD: {EUR: 0.93, BRL: 5.71},
+        BRL: {EUR: 0.16, USD: 0.18},
+        EUR: {BRL: 6.16, USD: 1.08}
     };
-
-
+ 
+    let valorConvertido;
+    if(damoeda === paramoeda){
+        valorConvertido = valor;
+    } else {
+        valorConvertido = valor * exchangeRates [damoeda][paramoeda];
+    }
+    let conversao = document.getElementById('conversao');
+    conversao.textContent = `Resultado ${valorConvertido.toFixed(2)} ${paramoeda}`;
 });
